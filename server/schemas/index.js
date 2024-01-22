5ef9836f622e31c350c02c0bc812c0dbb64a79ae
Aleesha Naxakis <aleesha-@hotmail.com>
2024-01-23 09:51:29 +1100

    added schemas directory and index.js file

---
+++ server/schemas/index.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server-express');
+const { someResolver, anotherResolver } = require('./resolvers');
+
+// Define your GraphQL schema (type definitions)
+const typeDefs = gql`
+  type Query {
+    hello: String
+    // Define your other queries here
+  }
+
+  type Mutation {
+    // Define your mutations here
+  }
+`;
+
+// Define your resolvers
+const resolvers = {
+  Query: {
+    hello: someResolver,
+    // Add other query resolvers here
+  },
+  Mutation: {
+    // Add mutation resolvers here
+  },
+};
+
+module.exports = { typeDefs, resolvers };
